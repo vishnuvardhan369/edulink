71702483073aa5f1d21beeb3a19139164e32504b
Vishnnuvardhan Chowdary Cheerla <153089937+vishnuvardhan369@users.noreply.github.com>
2025-08-24 10:30:08 +0530

    Update SearchPage.jsx

--- edulink-app/src/pages/SearchPage.jsx
+++ edulink-app/src/pages/SearchPage.jsx
@@ -23,7 +23,7 @@ export default function SearchPage({ navigateToProfile, navigateToHome }) {
             }
             setLoading(true);
             try {
-                const response = await fetch(`http://localhost:4000/api/users/search?query=${debouncedQuery}`);
+                const response = await fetch(`https://edulink-g0gqgxhhezfjbzg4.southindia-01.azurewebsites.net/api/users/search?query=${debouncedQuery}`);
                 const data = await response.json();
                 setResults(data);
             } catch (error) {
